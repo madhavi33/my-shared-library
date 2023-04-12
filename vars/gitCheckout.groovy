a19c43f0b9e50d324f3d8bfbf22f8d53d00549ba
madhavi33 <madhavi.v9@gmail.com>
2023-04-13 00:13:07 +0530

    commited

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,8 @@
 def call(Map stageParams) {
-
+ 
     checkout([
         $class: 'GitSCM',
-        branch: [(name:  stageParams.branch )],
-        userRemoteConfigs: [( url: stageParams.url )]
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
     ])
-}
+  }
