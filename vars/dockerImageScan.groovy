bd991d0b4e5650bb8e022444ad517e2ea93ff070
madhavi33 <madhavi.v9@gmail.com>
2023-04-13 00:18:22 +0530

    commited

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,9 +1,8 @@
+
 def call(String aws_account_id, String region, String ecr_repoName){
-
+    
     sh """
-     
-     trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
-     cat scan.txt
-
-     """
+    trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+    cat scan.txt
+    """
 }
