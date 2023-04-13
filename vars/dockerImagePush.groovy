85c9a415a1f7503621e85376acf57312047e2817
madhavi33 <madhavi.v9@gmail.com>
2023-04-13 13:28:21 +0530

    commited

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,8 +1,7 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-     //aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 089826821991.dkr.ecr.ap-south-1.amazonaws.com
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
