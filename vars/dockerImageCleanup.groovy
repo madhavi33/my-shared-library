6917c42e88f32e7f022d5e024494a881fa87a75f
madhavi33 <madhavi.v9@gmail.com>
2023-04-13 21:35:44 +0530

    commited

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -2,8 +2,7 @@ def call(String aws_account_id , String region, String ecr_repoName){
 
     sh """
     
-    
     docker rmi ${aws_account_id}.dkr.ecr.${region}.amazon.com/${ecr_repoName}:latest
-    
+       
     """
 }
